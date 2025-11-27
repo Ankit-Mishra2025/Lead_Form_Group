a7aaeae88a9910f366d6d03a7b3ce4f6533be83d
Ankit-Mishra2025 <devoankit2025@gmail.com>
2025-11-27 12:20:05 +0530

    slider fixed

--- src/SliderPage/FinanceSlider.jsx
+++ src/SliderPage/FinanceSlider.jsx
@@ -4,28 +4,24 @@ import "swiper/css";
 import "swiper/css/autoplay";
 import { Autoplay } from "swiper/modules";
 
-
 const FinanceSlider = () => {
+  const [financeImageData, setFinanceImageData] = useState([]);
 
+  const fetchFinanceImage = async () => {
+    try {
+      const data = await fetch("/FinanceSlideData/FinanceData.json");
+      const response = await data.json();
+      console.log(response);
 
-const[financeImageData,setFinanceImageData]=useState([])
+      setFinanceImageData(response);
+    } catch (error) {
+      console.log("Error is", error);
+    }
+  };
 
-const fetchFinanceImage=async()=>{
-try {
-  const data=await fetch("/FinanceSlideData/FinanceData.json")
-  const response=await data.json()
-  console.log(response);
-  
-  setFinanceImageData(response)
-} catch (error) {
-  console.log("Error is",error);
-  
-}
-}
-
-useEffect(()=>{
-fetchFinanceImage()
-},[])
+  useEffect(() => {
+    fetchFinanceImage();
+  }, []);
 
   return (
     <div className="w-full py-10 min-h-90">
@@ -40,37 +36,27 @@ fetchFinanceImage()
 
       <div className="mt-10">
         <Swiper
-          modules={[Autoplay]}
-          spaceBetween={20}
-          slidesPerView={5}
-          loop={true}
-          speed={5000}
-          autoplay={{
-            delay: 1,
-            disableOnInteraction: false,
-          }}
-          freeMode={true}
-          grabCursor={true}
-        >
+  modules={[Autoplay]}
+  spaceBetween={20}
+  slidesPerView={financeImageData.length < 5 ? financeImageData.length : 5}
+  loop={financeImageData.length > 5}
+  speed={2000}
+  autoplay={{
+    delay: 0,
+    disableOnInteraction: false,
+  }}
+  freeMode={true}
+  grabCursor={true}
+>
+  {financeImageData.map((item, idx) => (
+    <SwiperSlide key={idx.id}>
+      <div className="w-[200px] h-[100px] flex items-center justify-center bg-white shadow-lg rounded-xl p-5 flex-col">
+        <img src={item.image} className="h-[70px] w-[200px]" />
+      </div>
+    </SwiperSlide>
+  ))}
+</Swiper>
 
-
-
-          {
-            financeImageData.map((item,idx)=>{
-              return(
-                <div >
- <SwiperSlide key={idx} >
-              <div className="w-[200px] h-[100px] flex items-center justify-center bg-white shadow-lg   rounded-xl p-5 flex-col">
-                <img src={item.image} className="h-[70px] w-[200px]" />
-               
-              </div>
-            </SwiperSlide>
-                </div>
-              )
-            })
-          }
-          
-        </Swiper>
       </div>
     </div>
   );
